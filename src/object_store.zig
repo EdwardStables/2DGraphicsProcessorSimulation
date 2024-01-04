50b4b1dc6dbf0520def828480f1064218a3c6c27
Edward Stables <edward.stables1198@gmail.com>
2024-01-04 00:53:33 +0000

    store more testing and object adding

--- src/object_store.zig
+++ src/object_store.zig
@@ -28,6 +28,10 @@ pub const ObjectStore = struct {
         try self.objects.append(obj);
     }
 
+    pub fn addObject(self: *ObjectStore, object: types.Object) !void {
+        try self.objects.append(object);
+    }
+
     pub fn runBackend(self: *ObjectStore, manager: types.ManagerToStore) ?types.StoreToCoallesce {
         if (manager.kick_id != self.current_kick) {
             self.current_kick = manager.kick_id;
@@ -72,9 +76,70 @@ test "simple store test" {
     try expect(resp.kick_id == 2);
     try expect(std.meta.eql(resp.object, test_obj));
     try expect(store.runBackend(kick2) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
 
     resp = store.runBackend(kick3).?;
     try expect(resp.kick_id == 3);
     try expect(std.meta.eql(resp.object, test_obj));
     try expect(store.runBackend(kick3) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
+}
+
+test "multiple objects" {
+    var store = ObjectStore.init(ta);
+    defer store.deinit();
+
+    const obj1 = types.Object{ .x = 10, .y = 10, .width = 100, .height = 100 };
+    const obj2 = types.Object{ .x = 20, .y = 20, .width = 100, .height = 100 };
+    const obj3 = types.Object{ .x = 30, .y = 30, .width = 100, .height = 100 };
+
+    const kick = types.ManagerToStore{ .kick_id = 1 };
+
+    try expect(store.runBackend(kick) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
+
+    try store.addObject(obj1);
+
+    var resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj1));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 1);
+    try expect(store.runBackend(kick) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
+
+    try store.addObject(obj2);
+
+    resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj1));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 1);
+    resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj2));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 2);
+    try expect(store.runBackend(kick) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
+
+    try store.addObject(obj3);
+
+    resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj1));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 1);
+    resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj2));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 2);
+    resp = store.runBackend(kick).?;
+    try expect(std.meta.eql(resp.object, obj3));
+    try expect(store.current_kick == 1);
+    try expect(store.object_index == 3);
+    try expect(store.runBackend(kick) == null);
+    try expect(store.current_kick == 0);
+    try expect(store.object_index == 0);
 }
